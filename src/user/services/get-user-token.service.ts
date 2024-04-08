6f0d8121012d635de2434138e3c37691b8635680
gwonyeong <qwe980121@naver.com>
2024-04-08 22:54:02 +0900

    feat: 유저 토큰 발급 로직

--- src/user/services/get-user-token.service.ts
+++ src/user/services/get-user-token.service.ts
@@ -2,14 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { UserRepositoryPort } from './port/user.repository.port';
 
 @Injectable()
-export class GetUserTokenService {
-  constructor(private readonly userRepositoryPort: UserRepositoryPort) {}
-
-  public async getUserToken(userId: number) {
-    return await this.userRepositoryPort.getUserToken(userId);
+export class UserTokenService {
+  private readonly maxCount: number;
+  constructor(private readonly userRepositoryPort: UserRepositoryPort) {
+    this.maxCount = 10;
   }
 
-  public async createUserToken(userId: number) {
-    return await this.userRepositoryPort.createUserToken(userId);
+  public async getOneByUserId(userId: number) {
+    return await this.userRepositoryPort.getOne(userId);
+  }
+
+  public async getAll() {
+    return await this.userRepositoryPort.getAll();
+  }
+
+  public async create(userId: number) {
+    return await this.userRepositoryPort.create(userId);
+  }
+
+  public async createNewToken(userId: number, count: number) {}
+
+  public async getOrCreate(userId: number) {
+    const existUserToken = this.getOneByUserId(userId);
+
+    if (!existUserToken) {
+      const tokens = await this.getAll();
+      if (tokens.length >= this.maxCount) {
+      }
+    }
+    if (existUserToken) {
+      return existUserToken;
+    }
   }
 }
--- src/user/services/user.service.spec.ts
+++ src/user/services/user.service.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { GetUserTokenService } from './get-user-token.service';
+import { UserTokenService } from './get-user-token.service';
 import { UserRepositoryPort } from './port/user.repository.port';
 import { JwtService } from '@nestjs/jwt';
 import { UserToken } from '@prisma/client';
 
 describe('UserService', () => {
-  let service: GetUserTokenService;
+  let service: UserTokenService;
   let userRepositoryPort: UserRepositoryPort;
   let jwtService: JwtService;
 
   beforeEach(async () => {
     userRepositoryPort = {
-      getUserToken: jest.fn(async (userId: number): Promise<UserToken> => {
+      getOne: jest.fn(async (userId: number): Promise<UserToken> => {
         return {
           id: 1,
           token: 'token',
@@ -20,10 +20,11 @@ describe('UserService', () => {
           updatedAt: new Date(),
         };
       }),
-      createUserToken: jest.fn(),
+      getAll: jest.fn(),
+      create: jest.fn(),
     };
 
-    service = new GetUserTokenService(userRepositoryPort);
+    service = new UserTokenService(userRepositoryPort);
   });
 
   it('should be defined', () => {
@@ -32,15 +33,32 @@ describe('UserService', () => {
 
   describe('getUserToken', () => {
     it('repository의 getUserToken을 호출한다.', () => {
-      service.getUserToken(1);
-      expect(userRepositoryPort.getUserToken).toBeCalled();
+      service.getOneByUserId(1);
+      expect(userRepositoryPort.getOne).toBeCalled();
     });
   });
 
   describe('createUserToken', () => {
     it('repository의 createUserToken을 호출한다.', () => {
-      service.createUserToken(1);
-      expect(userRepositoryPort.createUserToken).toBeCalled();
+      service.create(1);
+      expect(userRepositoryPort.create).toBeCalled();
+    });
+  });
+
+  describe('createNewToken', () => {
+    it('jwt token을 생성한다.', () => {});
+  });
+
+  describe('getOrCreate', () => {
+    it('getUserToken을 호출한다.', () => {
+      service.getOrCreate(1);
+      expect(userRepositoryPort.getOne).toBeCalled();
+    });
+
+    it('token이 없으면 새로운 token을 생성한다.', () => {
+      jest.spyOn(service, 'getOneByUserId').mockResolvedValue(null);
+      service.getOrCreate(1);
+      expect(userRepositoryPort.create).toBeCalled();
     });
   });
 });
